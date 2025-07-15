27bf9462080b8dbb4775566c6388b14d37f3c518
mohit nandan <mohit.nandan@infinitelocus.com>
2025-07-15 17:34:46 +0530

    add middleware and improve login functionality

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,19 +1,33 @@
 import time
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class LoginPage(BasePage):
-    USERNAME = (By.XPATH, '//input[@id="email"]')  # adjust as needed
+    USERNAME = (By.XPATH, '//input[@id="email"]') 
     PASSWORD = (By.XPATH, "//input[@placeholder='Password']")
     Continue_with_email_btn = (By.XPATH, "//button[normalize-space()='Continue with Email']") 
-    LOGIN_BTN = (By.XPATH, "//button[normalize-space()='Login']")  # adjust as needed
+    LOGIN_BTN = (By.XPATH, "//button[normalize-space()='Login']")
+    WRONG_USERNAME = "abcd"
+    WRONG_PASSWORD = "test1234"
 
     def login(self, username, password):
-        self.type(*self.USERNAME, username)
-        time.sleep(1) 
+        wait = WebDriverWait(self.driver, 10)
+
+        self.type(*self.USERNAME, self.WRONG_USERNAME)
+        time.sleep(1)
         self.click(*self.Continue_with_email_btn)
-        time.sleep(1) 
+        time.sleep(1)
+
+
+        self.driver.refresh()
+
+
+        self.type(*self.USERNAME, username)
+        self.click(*self.Continue_with_email_btn)
+        wait.until(EC.presence_of_element_located(self.PASSWORD)) 
         self.type(*self.PASSWORD, password)
-        time.sleep(1) 
         self.click(*self.LOGIN_BTN)
-        time.sleep(1) 
+        time.sleep(1)
+
